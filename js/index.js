b3e2e97d8d53cc8d500a18631b4a21c10cfe9cab
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-20 22:38:32 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -2,3 +2,7 @@ var test = "testvalue";
 // stuff doesn't get read after an error, apparently.
 // the below is in error.
 // probably because "document" is an HTML function.
+var main = document.getElementById("main");
+main.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
