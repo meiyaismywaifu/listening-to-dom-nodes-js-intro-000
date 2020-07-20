c939e1e64ec52f27e669ce009155ef398cf1fb57
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-20 22:26:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var test = "testvalue";
+// stuff doesn't get read after an error, apparently.
+// the below is in error
 
-const main = document.getElementById('main');
+const main = index.document.getElementById('main');
 main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
