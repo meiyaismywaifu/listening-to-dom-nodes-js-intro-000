c132e679c7634bc2eea25f9fd22eebd6fc584d30
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-20 22:43:26 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -6,3 +6,9 @@ var main = document.getElementById("main");
 main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
+
+var input = document.querySelector('input');
+ 
+input.addEventListener('keydown', function(e) {
+  console.log(e.key);
+});
